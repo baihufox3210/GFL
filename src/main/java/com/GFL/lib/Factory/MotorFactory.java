4bfa7fa41b9206ec5bbe4db9ae0ad88598222ec4
baihu <baihu3210@gmail.com>
2026-04-19 11:40:39 +0800

    Add MotorFactory class for creating motor instances with configurable models

---
+++ src/main/java/com/GFL/lib/Factory/MotorFactory.java
@@ -0,0 +1,34 @@
+package com.GFL.lib.Factory;
+
+import com.GFL.lib.hardware.Motor.Spark.SparkFlexMotor;
+import com.GFL.lib.hardware.Motor.Spark.SparkMaxMotor;
+import com.GFL.lib.hardware.config.MotorConfig;
+import com.GFL.lib.hardware.interfaces.GenericMotor;
+
+public class MotorFactory {
+    @FunctionalInterface
+    public interface MotorConstructor {
+        GenericMotor create(int id, MotorModel motorModel, MotorConfig motorConfig);        
+    }
+
+    public enum MotorModel {
+        Neo550(20, 20, SparkMaxMotor::new),
+        Neo(40, 44, SparkMaxMotor::new),
+        NeoVortex(40, 44, SparkFlexMotor::new),;
+
+        public final int defaultStatorCurrentLimit;
+        public final int defaultSupplyCurrentLimit;
+        
+        public final MotorConstructor constructor;
+
+        MotorModel(int defaultStatorCurrentLimit, int defaultSupplyCurrentLimit, MotorConstructor constructor) {
+            this.defaultStatorCurrentLimit = defaultStatorCurrentLimit;
+            this.defaultSupplyCurrentLimit = defaultSupplyCurrentLimit;
+            this.constructor = constructor;
+        }
+    }
+
+    public static GenericMotor createMotor(int id, MotorModel model, MotorConfig motorConfig) {
+        return model.constructor.create(id, model, motorConfig);
+    }
+}
